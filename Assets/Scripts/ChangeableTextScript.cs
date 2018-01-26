f63f5fbd02531c83f2292583b53991315a16888c
YOSICOMP\yosi1202 <yosi1202@gmail.com>
2018-01-26 13:23:17 +0200

    yosi changeable text

---
+++ Assets/Scripts/ChangeableTextScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeableTextScript : MonoBehaviour {
+
+    [SerializeField] Text text;
+    [SerializeField] List<string> options;
+
+    string selected;
+    int count = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        text.text = selected;
+
+    }
+
+    public void InitOptions(List<string> optionslist)
+    {
+        options = optionslist;
+
+        selected = options[0];
+    }
+
+    public void BtnUp()
+    {
+        count--;
+
+        if (count < 0)
+            count = options.Count - 1;
+
+        selected = options[count];
+    }
+
+    public void BtnDown()
+    {
+        count++;
+
+        if (count > options.Count - 1)
+            count = 0;
+
+        selected = options[count];
+    }
+}
