551bd7a6a99858bd9b97e0b6a74e3a2735cc9b26
NatalyCreates <nataly@natalycreates.com>
2018-01-26 19:47:35 +0200

    added code by lynne - ids for notes and some story flow

--- Assets/Scripts/Data/NoteData.cs
+++ Assets/Scripts/Data/NoteData.cs
@@ -3,14 +3,18 @@ using System.Text.RegularExpressions;
 
 [System.Serializable]
 public class NoteData {
-    
+
+    public int noteId;
     public Enums.Character author;
     public string content;
+    public bool triggersEndingCheck;
     public List<MadlibData> madlibs;
 
-    public NoteData(Enums.Character author, string content, List<MadlibData> madlibs) {
+    public NoteData(int noteId, Enums.Character author, string content, bool triggersEndingCheck, List<MadlibData> madlibs) {
+        this.noteId = noteId;
         this.author = author;
         this.content = content;
+        this.triggersEndingCheck = triggersEndingCheck;
         this.madlibs = madlibs;
     }
 }
